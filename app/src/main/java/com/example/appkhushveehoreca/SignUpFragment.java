9ca224963062f203af8baa1470cb8e61ced18245
khushank <mistry.khushank@gmail.com>
2021-03-20 00:47:31 +0530

    Commit-1

--- app/src/main/java/com/example/appkhushveehoreca/SignUpFragment.java
+++ app/src/main/java/com/example/appkhushveehoreca/SignUpFragment.java
@@ -1,5 +1,6 @@
 package com.example.appkhushveehoreca;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,11 +8,31 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUpFragment extends Fragment {
 
@@ -22,14 +43,38 @@ public class SignUpFragment extends Fragment {
     private TextView alreadyHaveAnAccount1;
     private FrameLayout parentFrameLayout;
 
+    private EditText email,name,mobileNumber,password;
+    private ImageButton closeBtn;
+    private Button signUpBtn;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
+    private String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+.[a-z]+";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+
         alreadyHaveAnAccount1 = view.findViewById(R.id.alreadyHaveAnAccount);
         parentFrameLayout = getActivity().findViewById(R.id.registerFrameLayout);
+
+        email = view.findViewById(R.id.signUpEmail);
+        name = view.findViewById(R.id.singUpFullName);
+        mobileNumber = view.findViewById(R.id.signUpNumber);
+        password = view.findViewById(R.id.signUpPassword);
+
+        closeBtn = view.findViewById(R.id.signUpCloseBtn);
+        signUpBtn = view.findViewById(R.id.singUpBtn);
+
+        progressBar = view.findViewById(R.id.singUpProgressBar);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+
         return view;
     }
 
@@ -40,6 +85,82 @@ public class SignUpFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 setFragment(new SingInFragment());
+            }
+        });
+
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInputs();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mobileNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /// todo send data to Firebase
+                checkEmailAndPassword();
 
             }
         });
@@ -48,5 +169,82 @@ public class SignUpFragment extends Fragment {
         FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(parentFrameLayout.getId(),fragment);
         fragmentTransaction.commit();
+    }
+
+    private void checkInputs() {
+        if(!TextUtils.isEmpty(email.getText())){
+            if(!TextUtils.isEmpty(name.getText())){
+                if(!TextUtils.isEmpty(mobileNumber.getText()) && mobileNumber.length() >=10){
+                    if(!TextUtils.isEmpty(password.getText()) && password.length() >=8){
+                        signUpBtn.setEnabled(true);
+                    }else{
+                        signUpBtn.setEnabled(true);
+                    }
+                }else{
+                    signUpBtn.setEnabled(true);
+                }
+            }else{
+                signUpBtn.setEnabled(true);
+            }
+        }else{
+            signUpBtn.setEnabled(false);
+        }
+    }
+
+    private void checkEmailAndPassword(){
+
+
+        /// Create drawable here for change in error icon ///
+
+
+        if(email.getText().toString().matches(emailPattern)){
+
+            progressBar.setVisibility(View.VISIBLE);
+            signUpBtn.setEnabled(false);
+
+            firebaseAuth.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString())
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+
+                                Map<Object,String> userdata = new HashMap<>();
+                                userdata.put("fullname",name.getText().toString());
+                                userdata.put("mobilenumber",mobileNumber.getText().toString());
+
+                                firebaseFirestore.collection("USERS")
+                                        .add(userdata)
+                                        .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<DocumentReference> task) {
+                                                if(task.isSuccessful()){
+
+                                                    Intent mainIntent = new Intent(getActivity(),MainActivity.class);
+                                                    startActivity(mainIntent);
+                                                    getActivity().finish();
+
+                                                }else{
+                                                    progressBar.setVisibility(View.INVISIBLE);
+                                                    signUpBtn.setEnabled(true);
+                                                    String error = task.getException().getMessage();
+                                                    Toast.makeText(getActivity(), error, Toast.LENGTH_SHORT).show();
+
+                                                }
+                                            }
+                                        });
+                            }else{
+                                progressBar.setVisibility(View.INVISIBLE);
+                                signUpBtn.setEnabled(true);
+                                String error = task.getException().getMessage();
+                                Toast.makeText(getActivity(), error, Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+        }else{
+            email.setError("Invalid Email!");
+
+        }
+
     }
 }
