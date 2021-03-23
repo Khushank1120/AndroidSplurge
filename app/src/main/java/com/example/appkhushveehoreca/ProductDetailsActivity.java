34b2aa98494fb8da9b2406757998edf1f663c822
khushank <mistry.khushank@gmail.com>
2021-03-23 18:15:15 +0530

    Commit-13

--- app/src/main/java/com/example/appkhushveehoreca/ProductDetailsActivity.java
+++ app/src/main/java/com/example/appkhushveehoreca/ProductDetailsActivity.java
@@ -1,14 +1,74 @@
 package com.example.appkhushveehoreca;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductDetailsActivity extends AppCompatActivity {
+
+    private ViewPager productImagesViewPager;
+    private TabLayout viewPagerIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        productImagesViewPager = findViewById(R.id.product_images_viewpager);
+        viewPagerIndicator = findViewById(R.id.viewpager_indicator);
+
+        List<Integer> productImages = new ArrayList<>();
+               productImages.add(R.drawable.steelone);
+               productImages.add(R.drawable.steeltwo);
+               productImages.add(R.drawable.steelthree);
+               productImages.add(R.drawable.steelfour);
+
+               ProductImagesAdapter productImagesAdapter = new ProductImagesAdapter(productImages);
+               productImagesViewPager.setAdapter(productImagesAdapter);
+
+        viewPagerIndicator.setupWithViewPager(productImagesViewPager,true);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.search_cart_icon, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            finish();
+            return true;
+        }else if(id == R.id.main_search_icon){
+            //// todo: search
+            return true;
+        }else if (id == R.id.main_cart_icon){
+
+            /// todo: cart
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
