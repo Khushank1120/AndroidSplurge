b790f332f39ca55e9c4b90ceb1718fc563edfe48
khushank <mistry.khushank@gmail.com>
2021-04-28 19:41:53 +0530

    Commit-24

--- app/src/main/java/com/example/appkhushveehoreca/ActivityFirstBeforeSignUp.java
+++ app/src/main/java/com/example/appkhushveehoreca/ActivityFirstBeforeSignUp.java
@@ -10,5 +10,6 @@ public class ActivityFirstBeforeSignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_before_sign_up);
+
     }
 }
--- app/src/main/java/com/example/appkhushveehoreca/PlateModel.java
+++ app/src/main/java/com/example/appkhushveehoreca/PlateModel.java
@@ -1,4 +1,21 @@
 package com.example.appkhushveehoreca;
 
 public class PlateModel {
+    {
+        /// Empty Constructor ///
+    }
+
+    private int plate_img;
+
+    public PlateModel(int plate_img) {
+        this.plate_img = plate_img;
+    }
+
+    public int getPlate_img() {
+        return plate_img;
+    }
+
+    public void setPlate_img(int plate_img) {
+        this.plate_img = plate_img;
+    }
 }
