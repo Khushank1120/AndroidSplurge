d4a1b7147e33ff45ed3a7a539339e717b9bfd6e4
khushank <mistry.khushank@gmail.com>
2021-04-28 19:26:30 +0530

    Commit-23

---
+++ app/src/main/java/com/example/appkhushveehoreca/ActivityFirstBeforeSignUp.java
@@ -0,0 +1,14 @@
+package com.example.appkhushveehoreca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityFirstBeforeSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_before_sign_up);
+    }
+}
